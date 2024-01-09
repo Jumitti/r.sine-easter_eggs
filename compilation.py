6eaed6665e16b5b817ead08526c4f08ad6652a0b
Minniti Julien <minnitijulien06@gmail.com>
2024-01-09 10:26:30 +0100

    Update

---
+++ compilation.py
@@ -0,0 +1,16 @@
+file1_path = "output/unique_pictures.txt"
+file2_path = "output_v2/unique_pictures.txt"
+output_file_path = "compilation_unique_pictures.txt"
+
+with open(file1_path, 'r') as file1:
+    lines_file1 = set(file1.read().splitlines())
+
+with open(file2_path, 'r') as file2:
+    lines_file2 = set(file2.read().splitlines())
+
+merged_lines = lines_file1.union(lines_file2)
+
+with open(output_file_path, 'w') as output_file:
+    output_file.write('\n'.join(merged_lines))
+
+print(f"Fusion ended successfully : {output_file_path}")
